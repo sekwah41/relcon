f11d668764d1c8f4068abfab23611840da675b8b
Sekwah <contact@sekwah.com>
2026-07-06 02:24:05 +0100

    chore: add clap library for getting the args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    #[arg(long)]
+    path: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let working_dir = args.path.unwrap_or_else(|| env::current_dir().expect("failed to determine current directory"));
+
+    // let gix = gix::open(&working_dir);
+    println!("Working directory: {}", working_dir.display());
 }
